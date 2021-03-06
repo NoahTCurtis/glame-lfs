7dfe3cc64b71af73759028cc3a128124621d7657
Noah Curtis <noahtcurtis@gmail.com>
2021-03-05 23:35:57 -0800

    Added editor folder in scripts folder

---
+++ Assets/Scripts/Editor/MultiChunkEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Voxel
+{
+	[CustomEditor(typeof(MultiChunk))]
+	[CanEditMultipleObjects]
+	public class MultiChunkEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			MultiChunk mc = target as MultiChunk;
+
+			if(GUILayout.Button("Generate"))
+			{
+				mc.Generate(true);
+			}
+
+			if(GUILayout.Button("Clear"))
+			{
+				mc.HardReset();
+			}
+
+			DrawDefaultInspector();
+		}
+	}
+}
